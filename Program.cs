e0e8353996aac5e1589044e93127141047503d5c
hp15f <hp15f@LAPTOP-1G0PTJV1>
2024-11-27 07:40:22 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Please input a number");
+int number = int.Parse(Console.ReadLine());
+
+if (number <= 0)
+{
+    Console.WriteLine("Invalid input data");
+}
+else
+{
+    Console.WriteLine("Collatz Conjecture:");
+    while (number != 1)
+    {
+        Console.WriteLine(number);
+        if (number % 2 == 0)
+        {
+            number = number / 2;
+        }
+        else
+        {
+            number = (number * 3) + 1;
+        }
+    }
+    Console.WriteLine(number);
+}
+Console.ReadLine();
